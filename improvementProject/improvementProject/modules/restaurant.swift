e380a9742860902a6888d6bbc2336b86ac6241d7
ffff1430 <wwww217039@gmail.com>
2020-08-24 16:34:41 +0800

    feat: 新增解json

--- improvementProject/improvementProject/modules/restaurant.swift
+++ improvementProject/improvementProject/modules/restaurant.swift
@@ -8,10 +8,29 @@
 
 import Foundation
 
-struct Restaurant {
+struct Restaurant: Codable {
     var name: String?
     var type: String?
     var location: String?
     var image: String?
     var isVisited: String?
+}
+
+class RestaurantDecodable{
+    
+    var restaurant: [Restaurant] = []
+
+private func loadTWA00() {
+    if let fileLocation = Bundle.main.url(forResource: "TWA00", withExtension: "json") {
+        do {
+            let data = try Data(contentsOf: fileLocation)
+            let jsonDecode = JSONDecoder()
+            let dataFromJson = try jsonDecode.decode([Restaurant].self, from: data)
+            self.restaurant = dataFromJson
+        } catch {
+            print("error")
+        }
+    }
+}
+
 }
