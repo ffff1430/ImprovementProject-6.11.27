49d981862eb02aaa65dfd1bd6b7f1883bde8a7f7
ffff1430 <wwww217039@gmail.com>
2020-08-24 16:21:39 +0800

    feat: 建立一個Restaurant的物件

---
+++ improvementProject/improvementProject/modules/restaurant.swift
@@ -0,0 +1,17 @@
+//
+//  restaurant.swift
+//  improvementProject
+//
+//  Created by chen yue on 2020/8/24.
+//  Copyright © 2020 chen yue. All rights reserved.
+//
+
+import Foundation
+
+struct Restaurant {
+    var name: String?
+    var type: String?
+    var location: String?
+    var image: String?
+    var isVisited: String?
+}
